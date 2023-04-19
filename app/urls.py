5a068a47d42ac47568d2a0fb13c93caac4536769
krishnal mahajan <112457317+km-79@users.noreply.github.com>
2023-04-19 22:51:33 +0530

    adding a mutltple images

--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path,include
 from .import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns= [
     path("",views.first),
     path("category/<slug:val>",views.Categoriview.as_view(),name="category"),
-    path("signup",views.register),
-    path("signin",views.login),
-
-
-]
+    path("product-detail/<int:pk>",views.ProductDetial.as_view(),name="product-detail")
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- app/views.py
+++ app/views.py
@@ -1,7 +1,9 @@
+from django.db.models import Count
 from django.shortcuts import render
 from django.shortcuts import HttpResponse 
 from django.views import View
 from urllib import request
+from .models import Product
 
 # Create your views here.
 def first(request):
@@ -9,7 +11,15 @@ def first(request):
 
 class  Categoriview(View):
     def get(self,request,val):
+        product= Product.objects.filter(category=val)
+        title=Product.objects.filter(category=val).values('title').annotate(total=Count('title'))
         return render(request,"app/category.html",locals())
+
+class ProductDetial(View):
+    def get(self,request,pk):
+        product=Product.objects.get(pk=pk)
+        return render(request,"app/Productdetail.html",locals())
+
     
 def  register(request):
     return render(request,"app/signup.html")
